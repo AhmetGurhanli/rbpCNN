102258ce869ac47dabc89c1132e5b282475f071f
Ahmet Gürhanlı <114232098+AhmetGurhanli@users.noreply.github.com>
2025-10-08 20:36:59 +0300

    Add files via upload

---
+++ rbpCNN_21channel_Under_Review.py
@@ -0,0 +1,9 @@
+"""
+Source Code for the paper "rbpCNN: A physics-informed deep learning model 
+for predicting piRNA and mRNA interactions"
+
+This work is under review in an academic journal. Whole source code 
+will be available here after the article gets accepted.
+
+"""
+
